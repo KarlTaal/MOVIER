ba0da80d65ff0aa53d4e349ecaee381c8f7fcd41
Artjom <valart99@gmail.com>
2020-12-01 18:34:01 +0200

    Remade responses, possible to add genres and like movies

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -26,6 +26,13 @@ async function addGenre(roomId, genres, admin) {
   }
 }
 
+async function getGenresInRoom(roomId, admin) {
+  const db = admin.firestore();
+  const room = db.collection('rooms').doc(roomId);
+  const selectedRoom = await room.get();
+  return selectedRoom.exists ? selectedRoom.data().genres : null;
+}
+
 async function createRoom(admin) {
   const db = admin.firestore();
   let roomId = generateId();
@@ -36,7 +43,12 @@ async function createRoom(admin) {
     room = db.collection('rooms').doc(roomId);
     createdRoom = await room.get();
   }
-  await room.set({'userCount': 1, 'genres': null});
+  await room.set({
+      'userCount': 1,
+      'genres': null,
+      'likes': {}
+    }
+  );
   return roomId;
 }
 
@@ -53,12 +65,27 @@ async function joinRoom(admin, roomId) {
   return 400;
 }
 
-const appRouter = (app, fs, admin) => {
+async function likeMovie(admin, roomId, movieId) {
   const db = admin.firestore();
+  let room = db.collection('rooms').doc(roomId)
+  const selectedRoom = await room.get();
+  if (selectedRoom.exists) {
+    const likes = selectedRoom.data().likes;
+    const userCount = selectedRoom.data().userCount;
+    likes[movieId] = (likes[movieId] || 0) + 1;
+    await room.update({
+      'likes': likes
+    })
+    return likes[movieId] === userCount;
+  }
+  return null;
+}
+
+const appRouter = (app, fs, admin) => {
 
   app.post("/create", (req, res) => {
     createRoom(admin).then(r => {
-      res.status(200).send(r)
+      res.status(200).json({room :r})
     })
   });
 
@@ -66,9 +93,9 @@ const appRouter = (app, fs, admin) => {
     const roomId = req.params["roomId"];
     joinRoom(admin, roomId).then(r => {
       if(r === 200)
-        res.status(200).send('OK')
+        res.status(200).send({info: 'OK'})
       else
-        res.status(400).send("Room does not exist")
+        res.status(400).send({info: 'Room does not exist'})
     })
   });
 
@@ -78,10 +105,27 @@ const appRouter = (app, fs, admin) => {
     const roomId = req.params["roomId"];
     addGenre(roomId, list, admin).then(r => {
       if(r === 200)
-        res.status(200).send('OK')
+        res.status(200).send({info: 'OK'})
       else
-        res.status(400).send("Room does not exist")
+        res.status(400).send({info: 'Room does not exist'})
     })
+  });
+
+  app.get("/genres/:roomId", (req, res) => {
+    const roomId = req.params["roomId"];
+    const genresInRoom = getGenresInRoom(roomId, admin);
+    genresInRoom.then(genres => {
+      if (genres)
+        res.status(200).send({genres: genres})
+    });
+  });
+
+  app.post("/:roomId/like/:movieId", (req, res) => {
+    const roomId = req.params["roomId"];
+    const movieId = req.params["movieId"];
+    likeMovie(admin, roomId, movieId).then(r => {
+      res.status(200).send({match: r});
+    });
   });
 
 };
